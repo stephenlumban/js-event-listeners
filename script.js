4f4a69e0f978601c0a12c9fa1c617f9c9f2fdd81
stephenlumban <stephenl@n-compass.biz>
2024-10-21 11:23:05 +0800

    create addGlobalEventListener function

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const grandparent = document.querySelector(".grandparent")
-const parent = document.querySelector(".parent")
-const child = document.querySelector(".child")
+const divs = document.querySelectorAll("div")
 
-grandparent.addEventListener("click", e => {
-    console.log("Grandparent Capture")
-}, { capture: true })
-
-parent.addEventListener("click", e => {
-    console.log("Parent Capture")
-}, { capture: true }
-)
-
-child.addEventListener("click", e => {
-    console.log("Child 1")
+addGlobalEventListener("click", "div", e => {
+    console.log("hi")
 })
 
-document.addEventListener("click", e => {
-    console.log("Document 1")
-})
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e=> {
+        if(e.target.matches(selector)) callback (e)
+    })
+}
+
+const newDiv = document.createElement("div")
+newDiv.style.width = "200px"
+newDiv.style.height = "200px"
+newDiv.style.backgroundColor = "purple"
+document.body.append(newDiv)
